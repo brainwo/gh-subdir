d0fcbabad52ca6d3f2e90aecf99ec2a3326859de
Brian Wo <45139213+brainwo@users.noreply.github.com>
2022-10-30 22:43:43 +0800

    feat: completly rewritten using REST api

--- go.mod
+++ go.mod
@@ -3,12 +3,12 @@ module github.com/brainwo/gh-subfolder
 go 1.19
 
 require (
+	github.com/alexflint/go-arg v1.4.3
 	github.com/cli/go-gh v0.1.2
 	github.com/cli/shurcooL-graphql v0.0.2
 )
 
 require (
-	github.com/alexflint/go-arg v1.4.3 // indirect
 	github.com/alexflint/go-scalar v1.1.0 // indirect
 	github.com/cli/safeexec v1.0.0 // indirect
 	github.com/henvic/httpretty v0.0.6 // indirect
@@ -16,6 +16,7 @@ require (
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.12.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/thlib/go-timezone-local v0.0.0-20210907160436-ef149e42d28e // indirect
--- main.go
+++ main.go
@@ -3,50 +3,95 @@ package main
 import (
 	"fmt"
 	"log"
+
 	"strings"
 
 	arg "github.com/alexflint/go-arg"
 	"github.com/cli/go-gh"
-	graphql "github.com/cli/shurcooL-graphql"
 )
 
-func main() {
-	var args struct {
-		Repo string `arg:"positional"`
-	}
-	arg.MustParse(&args)
-	repo := strings.Split(args.Repo, "/")
-	if len(repo) != 2 {
-		fmt.Println("Error parsing repository name, use owner/repository format")
-		return
-	}
-
-	client, err := gh.GQLClient(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var query struct {
-		Repository struct {
-			Refs struct {
-				Nodes []struct {
-					Name string
-				}
-			} `graphql:"refs(refPrefix: $refPrefix, first: $first)"`
-			StargazerCount int
-		} `graphql:"repository(owner: $owner, name: $name)"`
-	}
-	variables := map[string]interface{}{
-		"first":     graphql.Int(100),
-		"refPrefix": graphql.String("refs/heads/"),
-		"owner":     graphql.String(repo[0]),
-		"name":      graphql.String(repo[1]),
-	}
-	err = client.Query("RepositoryIssues", &query, variables)
-	if err != nil {
-		fmt.Println("Repository not found")
-		log.Fatal(err)
-	}
-	fmt.Println(query.Repository.Refs.Nodes)
+type args struct {
+	Url         string `arg:"positional,required"`
+	Target      string `arg:"-t" help:"where to put the downloaded diretory"`
+	NoRecursive bool   `arg:"--no-recursive" help:"don't download recursively"`
+	Depth       int    `arg:"-d" default:"-1" help:"recursive download depth"`
+	AsZip       bool   `arg:"--as-zip" help:"download subdirectory as a zip file"`
 }
 
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+func (args) Version() string {
+	return "gh-subdir 0.1.0\nhttps://github.com/brainwo/gh-subdir"
+}
+
+func main() {
+	var args args
+	arg.MustParse(&args)
+
+	depth := args.Depth
+
+	if args.NoRecursive {
+		depth = 0
+	}
+
+	recursiveDownload(args.Url, depth)
+
+	//TODO: implement logic to bring the download from temporary directory to current directory
+	//TODO: implement logic to save directory as zip
+}
+
+// Recursively download a directory, once the depth reaches 0 it will not download the subdirectory.
+// If the depth is set to -1, it will download all the subfolder.
+func recursiveDownload(url string, depth int) {
+	schema := strings.SplitN(url, "/", 8)
+
+	if len(schema) < 6 {
+		log.Fatal("Invalid url, too short")
+	}
+
+	if schema[2] != "github.com" && schema[5] != "tree" {
+		log.Fatal("Invalid url")
+	}
+
+	owner := schema[3]
+	repo := schema[4]
+	path := schema[7]
+
+	client, err := gh.RESTClient(nil)
+	response := []struct {
+		Type         string
+		Name         string
+		Html_url     string
+		Download_url string
+	}{}
+
+	err = client.Get(fmt.Sprintf("repos/%s/%s/contents/%s", owner, repo, path), &response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, content := range response {
+		switch content.Type {
+		case "file":
+			downloadFile(content.Download_url)
+		case "dir":
+			if depth != 0 {
+				recursiveDownload(content.Html_url, depth-1)
+			}
+		// TODO: add supports for submodules and symlinks
+		default:
+			return
+		}
+	}
+}
+
+// Save file to temp directory
+// TODO: implement the downloadFile function
+func downloadFile(url string) {
+	schema := strings.SplitN(url, "/", 7)
+
+	repo := schema[4]
+	path := schema[6]
+
+	// TODO: make it use TMP environment variable
+	fmt.Printf("Downloading to: /tmp/gh-subdir/%s/%s\n", repo, path)
+	// TODO: make it work for private repositories as well
+}
